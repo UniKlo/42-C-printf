37f569e620dfc17de3defff8acf453ecad35138a
Khloe Hou <khou@e1z3r7p9.42.us.org>
2018-08-22 18:54:54 -0700

    8/22/17

--- Makefile
+++ Makefile
@@ -5,9 +5,62 @@
 #                                                     +:+ +:+         +:+      #
 #    By: khou <marvin@42.fr>                        +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
-#    Created: 2018/08/15 20:17:36 by khou              #+#    #+#              #
-#    Updated: 2018/08/15 20:17:54 by khou             ###   ########.fr        #
+#    Created: 2018/08/22 18:08:35 by khou              #+#    #+#              #
+#    Updated: 2018/08/22 18:51:08 by khou             ###   ########.fr        #
 #                                                                              #
 #******************************************************************************#
 
-$
+C = clang
+
+NAME = libftprintf.a
+
+FLAGS = -Wall -Wextra -Werror -O2
+
+LIBFT = libft
+
+DIR_S = srcs
+
+DIR_O = obj
+
+HEADER = include
+
+SOURCES = ft_printf.c \
+			main.c \
+
+SRCS = $(addprefix $(DIR_S)/,$(SOURCES))
+
+OBJS = $(addprefix $(DIR_O)/,$(SOURCES:.c=.o))
+
+all: $(NAME)
+
+$(NAME): $(OBJS)
+	@make -C $(LIBFT)
+	@cp libft/libft.a ./$(NAME)
+	@ar rc $(NAME) $(OBJS)
+	@ranlib $(NAME)
+
+$(DIR_O)/%.o: $(DIR_S)/%.c
+	@mkdir -p obj
+	@$(CC) $(FLAGS) -I $(HEADER) -o $@ -c $<
+
+norme:
+	norminette ./libft/
+	@echo
+	norminette ./$(HEADER)/
+	@echo
+	norminette ./$(DIR_S)/
+
+clean:
+	rm -f $(OBJS)
+	rm -rf $(DIR_O)
+	make clean -C $(LIBFT)
+
+fclean: clean
+	rm -f $(NAME)
+	rm -f srcs/*~
+	rm -f srcs/\#*\#
+	make fclean -C $(LIBFT)
+
+re: fclean all
+
+.PHONY: fclean re norme all clean
---
+++ include/printf.h
@@ -0,0 +1,51 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   printf.h                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: khou <marvin@42.fr>                        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2018/08/20 13:12:49 by khou              #+#    #+#             */
+/*   Updated: 2018/08/22 18:17:25 by khou             ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef PRINTF_H
+# define PRINTF_H
+
+/*
+** -------------------------- External Headers ---------------------------------
+*/
+
+# include "../libft/libft.h"
+# include <stdarg.h>
+# include <errno.h>
+# include <stdio.h>
+
+/*
+** ----------------------------- Union  Type  ----------------------------------
+*/
+union						u_type
+{
+	int						di;
+	short int				hdi;
+	signed char				hhdi;
+	long int				ldi;
+	long long int			lldi;
+	intmax_t				jdi;
+	unsigned int			uox;
+	unsigned short int		huox;
+	unsigned char			hhuox;
+	unsigned long int		luox;
+	unsigned long long int	lluox;
+	uintmax_t				juox;
+	size_t					zdiuox;
+//	wint_t					lc;
+	unsigned 				charc;
+	wchar_t					lc;
+	char					*s;
+	wchar_t					*ls;
+//	void					p;
+};
+
+#endif
