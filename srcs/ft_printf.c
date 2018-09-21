eef00c3239dd570cabc1768a22de7b2ff763be04
Khloe Hou <khou@e1z2r3p14.42.us.org>
2018-09-21 01:24:43 -0700

    a

--- srcs/ft_printf.c
+++ srcs/ft_printf.c
@@ -6,11 +6,12 @@
 /*   By: khou <marvin@42.fr>                        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/08/15 20:20:45 by khou              #+#    #+#             */
-/*   Updated: 2018/08/29 02:20:13 by khou             ###   ########.fr       */
+/*   Updated: 2018/09/21 01:23:59 by khou             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "printf.h"
+#include "libft.h"
 
 int		ft_printf(const char * format, ...)
 {
