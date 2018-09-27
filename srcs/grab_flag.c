befaa2f4c255559dbaa77d28760476af7cd1fc33
Khloe Hou <khou@e1z4r13p1.42.us.org>
2018-09-26 23:31:07 -0700

    09/26/2018

--- srcs/grab_flag.c
+++ srcs/grab_flag.c
@@ -6,7 +6,7 @@
 /*   By: khou <marvin@42.fr>                        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/09/08 18:43:39 by khou              #+#    #+#             */
-/*   Updated: 2018/09/26 23:26:44 by khou             ###   ########.fr       */
+/*   Updated: 2018/09/26 23:30:21 by khou             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -90,7 +90,8 @@ int	specifier(t_block *blk, char c)//work, 1: why static 2.can combine?
 
 	if ((c == 's' || c == 'S' || c == 'p' || c == 'd' || c == 'D' || c == 'i' ||
 		 c == 'o' || c == 'O' || c == 'u' || c == 'U' || c == 'x' || c == 'X' ||
-		 c == 'c' || c == 'C' || c == '%' || c == 'Z' || c == 'b' || c == '*') &&
+		 c == 'c' || c == 'C' || c == '%' || c == 'Z' || c == 'b' || c == '*' ||
+		 c == 'n') &&
 		(blk->specifier = c))
 		return (1);
 //	printf("invalid directive from spe: %c\n", c);//not valid char
